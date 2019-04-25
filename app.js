85136a752dd1dbeadfe02c5d8be26da45a64f6e5
m-tartag <mattbrs51@gmail.com>
2019-04-25 10:20:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+function handleClick(){
+
+  const input = document.getElementById("input").value;
+  
+  const url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=${input}&format=json`
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      displayResults(response);
+    })
+  }
+
+function displayResults(myResults){
+  const displayResults = myResults[3].map(function(item, index){
+    
+    const title = myResults[1][index];
+    const content = myResults[2][index];
+    
+    return `
+    
+    <li>
+    <a href=${item}>${title}</a>
+    </li>
+    <p>${content}</p>
+    <br>`;
+
+  });
+
+  document.getElementById("results").innerHTML = displayResults.join("");
+};
